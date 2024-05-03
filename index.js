6221e694628ee7174cf9f0cf9885817f241de62d
abi-v-j <abijoy611@gmail.com>
2024-05-03 14:04:09 +0530

    new

--- index.js
+++ index.js
@@ -4,11 +4,31 @@ const app = express(); // Create an Express application instance
 const cors = require("cors"); // Cors for enabling Cross-Origin Resource Sharing
 const bodyParser = require("body-parser"); // Body parser for parsing incoming request bodies
 const port = 5000; // Port number for the server to listen on
+const mysql = require("mysql2");
 
 // Middleware setup
 app.use(cors()); // Enable CORS for all routes
 app.use(bodyParser.json()); // Parse incoming JSON requests
 app.use(bodyParser.urlencoded({ extended: true })); // Parse URL-encoded request bodies
+
+
+
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "",
+  port: 3306,
+});
+
+// Check Database Connection
+
+db.connect((err) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log("Database Connected");
+});
 
 
 // Start the server
@@ -20,5 +40,22 @@ app.listen(port, () => {
 app.get("/Example", (req, res) => {
   res.send({
     message: "Hai", // Send a simple JSON response with a message
+  });
+});
+
+
+app.post("/District", (req, res) => {
+  let qry3 =
+    "insert into tbl_district (district_name) values('" +
+    req.body.district_name +
+    "')";
+  db.query(qry3, (err, result) => {
+    if (err) {
+      console.log("Error");
+    } else {
+      res.send({
+        message: "Data Saved",
+      });
+    }
   });
 });
